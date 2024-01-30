82bea4403f78959c04897fd990b3df140381247e
sara <saramustafa1842002@gmail.com>
2024-01-30 08:11:00 +0200

    insert a node as a left of another node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,4 +1,10 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_insert_left - insert a node as a left child of another node
+ * @parent: pointer to the node to insert child in
+ * @value: value to store in newnode
+ * Return: pointer to the created node or NULL on faliure or if parent is NULL
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
@@ -11,7 +17,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
-		parent->left->parnet = new_node;
+		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
 	return (new_node);
