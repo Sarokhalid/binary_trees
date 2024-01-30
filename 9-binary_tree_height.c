2ea5aea646c98e830f9a6306e8d81661646ac067
sara <saramustafa1842002@gmail.com>
2024-01-30 18:25:14 +0200

    measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,11 +7,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
+
 	if (tree == NULL)
 		return (0);
-
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
+	if (tree->left != NULL)
+		left_height = binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		right_height = binary_tree_height(tree->right);
 	return ((left_height > right_height) ? left_height + 1 : right_height + 1);
 }
